dcd27c40d7b5db10c2d46f8dbac7100d754d8c6a
Mustafa <ravadies@protonmail.com>
2024-09-08 12:49:56 +0300

    Adding the intro and inputs and i will working in results

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     
     // intro
@@ -5,6 +6,31 @@ fn main() {
     println!("The Calculator Program");
     println!("------------------");
 
-    //
+    // inputs
+    println!("Please enter the first Number");
+
+    let mut number1 = String::new();
+    io::stdin()
+        .read_line(&mut number1)
+        .expect("Failed to read number");
+
+    println!("Please enter the Second Number");
+
+    let mut number2 = String::new();
+    io::stdin()
+        .read_line(&mut number2)
+        .expect("Failed to read number");
+    
+    // Results
+    println!("Select your process (| x | - | + | / |)");
+    let mut process = String::new();
+    io::stdin()
+        .read_line(&mut process)
+        .expect("Failed to read process");
+    
+
+    
+
+
 
 }
